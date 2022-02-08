a14d2789622874893f403f6e7943c39bc01ed507
Fernanda Pereira <ferprr@github.com>
2022-02-08 17:46:49 -0300

    first commit

---
+++ src/main/java/com/dcc/urnaeletronica/UrnaEletronicaApplication.java
@@ -0,0 +1,13 @@
+package com.dcc.urnaeletronica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrnaEletronicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrnaEletronicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dcc/urnaeletronica/UrnaEletronicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcc.urnaeletronica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrnaEletronicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
