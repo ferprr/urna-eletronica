64f0d10f6f93ddc7fcb0e6fcbdaa312770e5e24f
Fernanda Pereira <fernandacarolina.fcsp@gmail.com>
2022-02-16 18:01:44 -0300

    tratado lançamento de exceção para administrador

--- src/main/java/com/dcc/urnaeletronica/dao/DaoAdministrador.java
+++ src/main/java/com/dcc/urnaeletronica/dao/DaoAdministrador.java
@@ -3,10 +3,12 @@ package com.dcc.urnaeletronica.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 import com.dcc.urnaeletronica.model.Administrador;
 
 @Repository
 public interface DaoAdministrador extends JpaRepository<Administrador, Long>
 {
-	public Administrador findByUsernameAndSenha(String username, String senha);
+	public Optional<Administrador> findByUsernameAndSenha(String username, String senha);
 }
