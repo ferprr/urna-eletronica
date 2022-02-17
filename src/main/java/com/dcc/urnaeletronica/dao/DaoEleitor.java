80b9eb6a8fee616b55dda8a9c09e60ec4f97321c
Fernanda Pereira <fernandacarolina.fcsp@gmail.com>
2022-02-16 22:28:27 -0300

    alterando tipo titulo

--- src/main/java/com/dcc/urnaeletronica/dao/DaoEleitor.java
+++ src/main/java/com/dcc/urnaeletronica/dao/DaoEleitor.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DaoEleitor extends JpaRepository<Eleitor, Long>
+public interface DaoEleitor extends JpaRepository<Eleitor, String>
 {
 	public Optional<Eleitor> findByTituloEleitor(String tituloEleitor);
 }
