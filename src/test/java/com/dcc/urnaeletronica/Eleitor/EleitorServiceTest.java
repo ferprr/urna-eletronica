f301166588bbfa2dfc0a4c3cfcf029354c33c442
Fernanda Pereira <fernandacarolina.fcsp@gmail.com>
2022-02-16 22:28:48 -0300

    refatorando testes para eleitor

--- src/test/java/com/dcc/urnaeletronica/Eleitor/EleitorServiceTest.java
+++ src/test/java/com/dcc/urnaeletronica/Eleitor/EleitorServiceTest.java
@@ -1,10 +1,24 @@
 package com.dcc.urnaeletronica.Eleitor;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
 import com.dcc.urnaeletronica.dao.DaoEleitor;
+import com.dcc.urnaeletronica.exceptions.EleitorServiceException;
 import com.dcc.urnaeletronica.model.Eleitor;
 import com.dcc.urnaeletronica.service.EleitorService;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -25,64 +39,46 @@ public class EleitorServiceTest {
         eleitor = new Eleitor("0000000001", 222, 001, true);
     }
 
-    // @Test
-    // void testAutenticar() throws EleitorServiceException {
+    @Test
+    void testAutenticar() throws EleitorServiceException {
 
-    //     when(this.daoEleitor.findByTituloEleitor(anyString())).thenReturn(Optional.of(eleitor));
+        when(this.daoEleitor.findByTituloEleitor(anyString())).thenReturn(Optional.of(eleitor));
 
-    //     final Eleitor eleitorResponse = this.eleitorService.autenticar("0000000001");
+        final Eleitor eleitorResponse = this.eleitorService.autenticar("0000000001");
         
-    //     assertNotNull(eleitorResponse);
+        assertNotNull(eleitorResponse);
 
-    //     verify(this.daoEleitor, times(1)).findByTituloEleitor(anyString());
-    // }
+        verify(this.daoEleitor, times(1)).findByTituloEleitor(anyString());
+    }
 
-    // @Test
-    // void testAutenticarEleitorNaoEncontrado() {
-    //     Exception exception = assertThrows(EleitorServiceException.class, () -> {
-    //         this.eleitorService.autenticar(anyString());
-    //     });
+    @Test
+    void testAutenticarEleitorNaoEncontrado() {
+        Exception exception = assertThrows(EleitorServiceException.class, () -> {
+            this.eleitorService.autenticar(anyString());
+        });
         
-    //     assertEquals("Eleitor não encontrado.", exception.getMessage());
-    // }
+        assertEquals("Eleitor não encontrado.", exception.getMessage());
+    }
 
-    // @Test
-    // void testMarcaQueVotou() throws EleitorServiceException {
+    @Test
+    void testMarcaQueVotou() throws EleitorServiceException {
 
-    //     doNothing().when(this.daoEleitor.findByTituloEleitor(anyString()));
-    //     doNothing().when(this.daoEleitor.save(any(Eleitor.class)));
+        when(this.daoEleitor.findByTituloEleitor(anyString())).thenReturn(Optional.of(eleitor));
+        when(this.daoEleitor.save(any(Eleitor.class))).thenReturn(eleitor);
 
-    //     this.eleitorService.marcaQueVotou(anyString());
+        this.eleitorService.marcaQueVotou(anyString());
 
-    //     verify(this.daoEleitor, times(1)).findByTituloEleitor(anyString());
-    //     verify(this.daoEleitor, times(1)).save(any(Eleitor.class);
-    // }
+        verify(this.daoEleitor, times(1)).findByTituloEleitor(anyString());
+        verify(this.daoEleitor, times(1)).save(any(Eleitor.class));
+    }
 
-    // @Test
-    // void testMarcaQueVotouEleitorNaoEncontrado() {
-    //     Exception exception = assertThrows(EleitorServiceException.class, () -> {
-    //         this.eleitorService.marcaQueVotou(anyString());
-    //     });
-        
-    //     assertEquals("Eleitor não encontrado.", exception.getMessage());
-    // }
+    @Test
+    void testVerificaSeEleitorVotou() throws EleitorServiceException {
+        when(this.daoEleitor.findByTituloEleitor(anyString())).thenReturn(Optional.of(eleitor));
 
-    // @Test
-    // void testVerificaSeEleitorVotou() throws EleitorServiceException {
-    //     when(this.daoEleitor.findByTituloEleitor(anyString())).thenReturn(Optional.of(eleitor));
+        boolean votou = this.eleitorService.verificaSeEleitorVotou("0000000001");
+        assertTrue(votou);
 
-    //     boolean votou = this.eleitorService.verificaSeEleitorVotou("0000000001");
-    //     assertTrue(votou);
-
-    //     verify(this.daoEleitor, times(1)).findByTituloEleitor(anyString());
-    // }
-
-    // @Test
-    // void testVerificaSeEleitorVotouEleitorNaoEncontrado() {
-    //     Exception exception = assertThrows(EleitorServiceException.class, () -> {
-    //         this.eleitorService.verificaSeEleitorVotou(anyString());
-    //     });
-        
-    //     assertEquals("Eleitor não encontrado.", exception.getMessage());
-    // }
+        verify(this.daoEleitor, times(1)).findByTituloEleitor(anyString());
+    }
 }
