a3fef3bfce4d38ec54d5681c7e29490d0341d05d
Fernanda Pereira <fernandacarolina.fcsp@gmail.com>
2022-02-16 21:37:10 -0300

    mantendo padrões de indentação

--- src/main/java/com/dcc/urnaeletronica/service/VotoService.java
+++ src/main/java/com/dcc/urnaeletronica/service/VotoService.java
@@ -13,8 +13,7 @@ import com.dcc.urnaeletronica.model.TipoVoto;
 import com.dcc.urnaeletronica.model.Voto;
 
 @Service
-public class VotoService
-{
+public class VotoService {
 
 	@Autowired
 	public DaoVoto repositorio;
@@ -22,44 +21,48 @@ public class VotoService
 	@Autowired
 	public DaoCandidato repositorioCandidato;
 
-	public void atribuiVotos(Eleicao eleicaoAtiva, Candidato primeiroSenador, Candidato segundoSenador, Candidato presidente, boolean votoPrimeiroSenadorBranco,boolean votoSegundoSenadorBranco, boolean votoPresidenteBranco)
-	{
+	public void atribuiVotos(Eleicao eleicaoAtiva, Candidato primeiroSenador, Candidato segundoSenador,
+			Candidato presidente, boolean votoPrimeiroSenadorBranco, boolean votoSegundoSenadorBranco,
+			boolean votoPresidenteBranco) {
+
 		Voto primeiroVoto = new Voto();
-		if(votoPrimeiroSenadorBranco)
-		{
+
+		if (votoPrimeiroSenadorBranco) {
 			primeiroVoto.setCandidato(null);
 			primeiroVoto.setTipoVoto(TipoVoto.BRANCO);
+		} else {
+			primeiroVoto.setCandidato(
+					primeiroSenador != null ? repositorioCandidato.findByNumero(primeiroSenador.getNumero()) : null);
+			primeiroVoto.setTipoVoto(primeiroSenador != null ? TipoVoto.VALIDO : TipoVoto.NULO);
 		}
-		else
-		{
-		primeiroVoto.setCandidato(primeiroSenador != null ? repositorioCandidato.findByNumero(primeiroSenador.getNumero()) : null);
-		primeiroVoto.setTipoVoto(primeiroSenador != null ? TipoVoto.VALIDO : TipoVoto.NULO);
-		}
+
 		primeiroVoto.setEleicao(eleicaoAtiva);
+
 		Voto segundoVoto = new Voto();
-		if(votoSegundoSenadorBranco)
-		{
+
+		if (votoSegundoSenadorBranco) {
 			segundoVoto.setCandidato(null);
 			segundoVoto.setTipoVoto(TipoVoto.BRANCO);
+		} else {
+			segundoVoto.setCandidato(
+					segundoSenador != null ? repositorioCandidato.findByNumero(segundoSenador.getNumero()) : null);
+			segundoVoto.setTipoVoto(segundoSenador != null ? TipoVoto.VALIDO : TipoVoto.NULO);
 		}
-		else 
-		{
-		segundoVoto.setCandidato(segundoSenador != null ? repositorioCandidato.findByNumero(segundoSenador.getNumero()) : null);
-		segundoVoto.setTipoVoto(segundoSenador != null ? TipoVoto.VALIDO : TipoVoto.NULO);
-		}
+
 		segundoVoto.setEleicao(eleicaoAtiva);
+
 		Voto terceiroVoto = new Voto();
-		if(votoPresidenteBranco)
-		{
+
+		if (votoPresidenteBranco) {
 			terceiroVoto.setCandidato(null);
 			terceiroVoto.setTipoVoto(TipoVoto.BRANCO);
-		}
-		else
-		{
-			terceiroVoto.setCandidato(presidente != null ? repositorioCandidato.findByNumero(presidente.getNumero()) : null);
+		} else {
+			terceiroVoto.setCandidato(
+					presidente != null ? repositorioCandidato.findByNumero(presidente.getNumero()) : null);
 			terceiroVoto.setTipoVoto(presidente != null ? TipoVoto.VALIDO : TipoVoto.NULO);
 		}
 		terceiroVoto.setEleicao(eleicaoAtiva);
+		
 		repositorio.saveAll(Arrays.asList(primeiroVoto, segundoVoto, terceiroVoto));
 	}
 
